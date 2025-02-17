df0ac87c5b870f3a1e22281c0a682e6dc46a7af1
satriasudirja <satriasadar122@gmail.com>
2025-02-18 00:43:28 +0700

    Update frontend submodule

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [message, setMessage] = useState("");
+
+    useEffect(() => {
+        axios.get("http://localhost:8000/api/test")
+            .then(res => setMessage(res.data.message))
+            .catch(err => console.error(err));
+    }, []);
+
+    return (
+        <div className="p-4 text-center">
+            <h1 className="text-2xl font-bold">Test API</h1>
+            <p>{message}</p>
+        </div>
+    );
 }
 
 export default App;
